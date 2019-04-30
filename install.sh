54d82cf0761db270cc033020767daba134ec17c6
JibinKoshy07 <jibinkoshy07@gmail.com>
2019-04-30 11:04:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install youtube-dl -y
+sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
+sudo chmod a+x /usr/local/bin/youtube-dl
+hash -r
 chmod 777 jibutube
 cp jibutube /usr/local/bin
