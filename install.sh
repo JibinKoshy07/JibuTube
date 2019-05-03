2bb6e3e99a10932f05bbce4bb7267c0dcf2f6150
JibinKoshy07 <jibinkoshy07@gmail.com>
2019-05-03 11:16:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+printf '\e[40;38;5;82m Downloading requirements. Please sit back and relax! \e[0m\n'
 sudo apt-get update -y
 sudo apt-get install youtube-dl -y
 sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
