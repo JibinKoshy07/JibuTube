c153d0b3455b106f911ad4b4f4369818a4e22fb1
Jibin Koshy <jibinkoshy07@gmail.com>
2022-08-07 17:40:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 printf '\e[40;38;5;82m Downloading requirements. Please sit back and relax! \e[0m\n'
 sudo apt-get update -y
 sudo apt-get install youtube-dl -y
+sudo apt install python -y
 sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+x /usr/local/bin/youtube-dl
 hash -r
-chmod 777 jibutube
-cp jibutube /usr/local/bin
+sudo chmod 777 jibutube
+sudo cp jibutube /usr/local/bin
 touch jibutube.sh
 cat jibutube >> jibutube.sh
 chmod +x jibutube.sh
